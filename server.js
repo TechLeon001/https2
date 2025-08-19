923500febdeaa618496f06518b30e01783825dad
TechLeon001 <leonardnaibei92@gmail.com>
2025-08-19 23:25:08 +0300

    added db

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get("/love",(req,res)=>{
+    res.send("<h1>${date()}<h1>");
+
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
